6f9135ca0acf88c9b80252a844eaec88deb85e0d
Edwin Goddard <edwin.goddard@gmail.com>
2015-07-16 23:52:43 +0100

    Comments added

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+# There are 100 cars
 cars = 100
+# Each car has 4 spaces
 space_in_a_car = 4.0
+# There are 30 drivers
 drivers = 30
+# Thre are 90 passengers
 passengers = 90
+# The number of cars not driven is the total number of cars minus the number of drivers
 cars_not_driven = cars - drivers
+# Each drive will drive a car
 cars_driven = drivers
+# The total car pool capacity is the number of driven cars multiplied by the spaces in the car
 carpool_capacity = cars_driven * space_in_a_car
+# The average passengers per car is number of passengers divided by the number of cars driven
 average_passengers_per_car = passengers / cars_driven
 
 
