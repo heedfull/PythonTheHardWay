aca8bdf858202259f984ee0b14ef826d1c74d73c
Edwin Goddard <edwin.goddard@gmail.com>
2015-07-16 23:40:14 +0100

    adding some floating numbers

---
+++ ex3FloatingNumbers.py
@@ -0,0 +1,28 @@
+print "I will now count my chickens;"
+
+# 30 divided by 6 and add on 25
+print "Hens", 25 + 30 / 6
+# 25 time 3 modulus of 4 subtracted from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# 4 modulus of 2 minus 1 divide by 4 adding and subtracting the other numbers
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# logical test is 3 plus 2 less than 5 minus 7
+print 3 + 2 < 5 - 7
+
+# add 3 and 2
+print "What is 3 + 2?", 3 + 2
+# subtract 7 from 5
+print " What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equaul?", 5 <= -2
