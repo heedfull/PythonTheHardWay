6373870651ae2ec77cd8d376b8d64e90b6ef6de5
Edwin Goddard <edwin.goddard@gmail.com>
2015-07-16 23:38:07 +0100

    Commenting

--- ex3.py
+++ ex3.py
@@ -1,17 +1,22 @@
 print "I will now count my chickens;"
 
+# 30 divided by 6 and add on 25
 print "Hens", 25 + 30 / 6
+# 25 time 3 modulus of 4 subtracted from 100
 print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
+# 4 modulus of 2 minus zero (integer math) adding and subtracting the other numbers
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
-
+# logical test is 3 plus 2 less than 5 minus 7
 print 3 + 2 < 5 - 7
 
+# add 3 and 2
 print "What is 3 + 2?", 3 + 2
+# subtract 7 from 5
 print " What is 5 - 7?", 5 - 7
 
 print "Oh, that's why it's False."
