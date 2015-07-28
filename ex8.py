67f5b669e89d8f09236363cae0e49cff7bc11092
Edwin Goddard <edwin.goddard@gmail.com>
2015-07-28 20:59:57 +0100

    Commenting

--- ex8.py
+++ ex8.py
@@ -1,9 +1,15 @@
+# create a string full of formatting characters
 formatter = "%r %r %r %r"
 
+# print numbers
 print formatter % (1, 2, 3, 4)
+# print strings
 print formatter % ("one", "two", "three", "four")
+# print booleans
 print formatter % (True, False, False, True)
+# print string of format characters
 print formatter % (formatter, formatter, formatter, formatter)
+# print out some strings including one with a single quote
 print formatter % (
   "I had this thing.",
   "That you could type up right.",
