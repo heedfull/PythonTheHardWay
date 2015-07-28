9f7fe53494b0e3b5fbd0654796140a54842191a4
Edwin Goddard <edwin.goddard@gmail.com>
2015-07-28 20:16:03 +0100

    Commenting code

--- ex7.py
+++ ex7.py
@@ -1,8 +1,13 @@
+# Print some text
 print "Mary had a little lamb."
+# Insert the word snow as a string into the string
 print "Its fleece was white as %s." % 'snow'
+# Print some text
 print "And everywhere that Mary went."
+# Print a string that has ten full stops
 print "." * 10 # what'd that do?
 
+# Create 12 strings each with on letter
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,6 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it ot see what happens
+# Join them all together, comma continues on the same line
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
